bf6bd19bdef6a5defca17f6c6d6f697eb8ab7bb5
Nataliya202 <natamarty2017@gmail.com>
2023-05-31 20:53:19 +0300

    update commit

---
+++ js/01-gallery.js
@@ -0,0 +1,48 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+const items = []
+
+galleryItems.forEach(element => {
+    const galleryItem = document.createElement('div')
+    galleryItem.className = 'gallery__item'
+    const galleryLink = document.createElement('a')
+    galleryLink.className = 'gallery__link'
+    galleryLink.href = element.original
+    const galleryImage = document.createElement('img')
+    galleryImage.className = 'gallery__image'
+    galleryImage.src = element.preview;
+    galleryImage.setAttribute('data-source', element.original)
+    galleryImage.alt = element.description;
+
+    galleryItem.append(galleryLink)
+    galleryLink.append(galleryImage)
+    items.push(galleryItem)
+})
+
+gallery.append(...items)
+
+gallery.addEventListener('click', evt => {
+    evt.preventDefault();
+    if (evt.target.nodeName !== 'IMG') {
+        return
+    }
+
+    const selectedImage = evt.target.getAttribute('data-source')
+
+    const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`)
+
+    instance.show()
+
+    gallery.addEventListener('keydown', evt=> {
+        if (evt.key === 'Escape') {
+            instance.close()
+        }
+    })
+})
+
+
